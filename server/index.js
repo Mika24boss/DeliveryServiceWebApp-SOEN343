45736cb595e7de7299e46122ab399c88aaeb7ee9
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2023-09-19 20:25:47 -0400

    Server and Firebase init
    Initialized server and firebase scripts.

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+require('dotenv').config();
+const { graphqlHTTP} = require('express-graphql')
+const schema = require('./schema/schema')
+const port = process.env.PORT || 8000
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(port, console.log(`Server running on port ${port}`))
